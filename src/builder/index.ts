f045e566bb302876b1687ba323629c33cc75f177
Dennis Schwartz <dennis@repositive.io>
2018-12-31 00:48:15 +0100

    Made file download more robust.

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,8 +1,8 @@
 import * as tar from 'tar';
 import * as request from 'request';
 import * as webpack from 'webpack';
-import { Request, ResponseToolkit, ResponseObject } from 'hapi';
-import { mkdirSync, readFileSync, statSync, createReadStream } from 'fs';
+import { Request, ResponseToolkit } from 'hapi';
+import { mkdirSync, readFileSync, createReadStream, rmdirSync, existsSync } from 'fs';
 import { execSync } from 'child_process';
 const REGISTRY_URL = 'http://registry.npmjs.org';
 const components_prefix = 'components/';
@@ -14,48 +14,42 @@ export interface ComponentInfo {
 }
 
 export function download(url: string, targetPath: string): Promise<string> {
-  return new Promise((res, rej) => {
+  return new Promise((resolve, reject) => {
     console.log(`Downloading tarball from ${url}...`);
-    const s = request(url).pipe(
-      tar.x({
-        strip: 1,
-        C: targetPath,
-      }),
-    );
-    s.on('end', () => res(targetPath));
+    const req = request
+      .get(url)
+      .on('response', (res) => {
+        if (res.statusCode === 200) {
+          const s = req.pipe(
+            tar.x({
+              strip: 1,
+              C: targetPath,
+            }),
+          );
+          s.on('end', () => resolve(targetPath));
+        } else {
+          reject(`Error downloading file at ${url}! ${res.statusCode} - ${res.statusMessage}`);
+        }
+      });
   });
 }
 
-function folderExists(path: string) {
-  try {
-      const stat = statSync(path);
-      return stat.isDirectory();
-  } catch (err) {
-      if (err.code === 'ENOENT') {
-          return false;
-      }
-      throw err;
-  }
-}
-
-export async function build(options: ComponentInfo, h: ResponseToolkit): Promise<ResponseObject> {
+export async function build(options: ComponentInfo, h: ResponseToolkit): Promise<{ built: boolean, path?: string }> {
   const { module_name, module_version } = options;
   const local_path = `${process.cwd()}/${components_prefix}${module_name}/${module_version}`;
-  // check if build folder exists
-  // TODO: Check if the build file actually exists! This could cause race-conditions
-  if (folderExists(local_path)) {
-    const stream = createReadStream(`${local_path}/biojs-build/${bundle_filename}`);
-    return h.response(stream)
-      .type('application/javascript')
-      .header('Content-type', 'application/javascript');
+  const bundle_path = `${local_path}/biojs-build/${bundle_filename}`;
+
+  if (existsSync(bundle_path)) {
+    return { built: true, path: bundle_path };
   }
+
   console.log(`No build available for ${module_name}@${module_version}`);
   console.log('Creating directory...');
   mkdirSync(local_path, { recursive: true });
   // download if not
   const module_path = module_name.indexOf('@') > -1 ? `${module_name}/` : '';
   const url = `${REGISTRY_URL}/${module_path}-/${module_name}-${module_version}.tgz`;
-  return download(url, local_path)
+  download(url, local_path)
     .then((downloaded_path: string) => {
       // run npm i
       execSync('npm i', { cwd: downloaded_path });
@@ -83,12 +77,26 @@ export async function build(options: ComponentInfo, h: ResponseToolkit): Promise
           throw new Error('Couldn\'t build component!');
         }
       });
-      // return link to build.js
-      return h.response(`${downloaded_path}/biojs-build/${bundle_filename}`);
+    })
+    .catch((err) => {
+      console.log('There was an error building the component!', err);
+      rmdirSync(local_path);
     });
+  return { built: false };
 }
 
 export async function handleRequest(req: Request, h: ResponseToolkit): Promise<any> {
   const query = req.query as any; // Compiler doesn't get this.
-  return build(query, h);
+  return build(query, h)
+    .then((res) => {
+      const { built, path } = res;
+      if (built && path) {
+        const stream = createReadStream(path);
+        return h.response(stream)
+          .type('application/javascript')
+          .header('Content-type', 'application/javascript');
+      } else {
+        return h.response(`building...`);
+      }
+    });
 }
