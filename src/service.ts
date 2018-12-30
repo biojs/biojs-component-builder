04298e69008e07b9eefc284471ed21c98aba2831
Dennis Schwartz <dennis@repositive.io>
2018-12-30 18:34:48 +0100

    Initial template for listening to component requests.

--- src/service.ts
+++ src/service.ts
@@ -1,11 +1,23 @@
 import * as config from 'config';
 import { build, BuildCmd } from './builder';
+import { connect } from 'amqplib';
 
 export default async function init({
   _config = config,
 }: {
-  _config?: typeof config,
-}): Promise<string> {
+    _config?: typeof config,
+  }): Promise<string> {
+
+  const conn = await connect('amqp://guest:guest@rabbit:5672');
+  conn.on('error', (err) => console.log(err));
+  conn.createChannel()
+    .then((ch) => {
+      return ch.assertQueue('biojs')
+        .then((queue) => {
+          console.log('queue!', queue);
+        });
+    });
+
   // const module_name = 'mplexviz-ngraph';
   const module_name = '@chgibb/angularplasmid';
   // const module_name = 'cytoscape';
