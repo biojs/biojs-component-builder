d9447611a9814a44acc86187dd992b31b3a27779
Dennis Schwartz <dennis@repositive.io>
2019-01-03 02:59:46 +0100

    [F] Add API and fix concurrency issue. (#4)
    * Start Hapi server
    
    * Connect existing build function to /build route.
    
    * Use GET instead of POST and hapi's own validation.
    
    * Return file content if build file exists.
    
    * Made file download more robust.
    
    * Return proper headers and status if bundle is not available.
    
    * Refactor building.
    
    * Removed comment.

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,83 +1,128 @@
 import * as tar from 'tar';
 import * as request from 'request';
 import * as webpack from 'webpack';
-import { mkdirSync, readFileSync, statSync } from 'fs';
-import { execSync } from 'child_process';
+import { promisify } from 'util';
+import { Request, ResponseToolkit } from 'hapi';
+import { mkdirSync, readFileSync, createReadStream, rmdirSync, existsSync } from 'fs';
+import { exec as execCB } from 'child_process';
+const exec = promisify(execCB);
 const REGISTRY_URL = 'http://registry.npmjs.org';
 const components_prefix = 'components/';
+const bundle_filename = 'main.js';
 
-export function download(url: string, targetPath: string): Promise<string> {
-  return new Promise((res, rej) => {
-    console.log(`Downloading tarball from ${url}...`);
-    const s = request(url).pipe(
-      tar.x({
-        strip: 1,
-        C: targetPath,
-      }),
-    );
-    s.on('end', () => res(targetPath));
-  });
-}
-
-export interface BuildCmd {
+export interface ComponentInfo {
   module_name: string;
   module_version: string;
 }
 
-function folderExists(path: string) {
-  try {
-      const stat = statSync(path);
-      return stat.isDirectory();
-  } catch (err) {
-      if (err.code === 'ENOENT') {
-          return false;
-      }
-      throw err;
-  }
+export function download(module_name: string, module_version: string, targetPath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const module_path = module_name.indexOf('@') > -1 ? `${module_name}/` : '';
+    const url = `${REGISTRY_URL}/${module_path}-/${module_name}-${module_version}.tgz`;
+    console.log(`Downloading tarball from ${url}...`);
+    const req = request
+      .get(url)
+      .on('response', (res) => {
+        if (res.statusCode === 200) {
+          const s = req.pipe(
+            tar.x({
+              strip: 1,
+              C: targetPath,
+            }),
+          );
+          s.on('end', () => {
+            console.log(`Done dowloading to ${targetPath}!`);
+            resolve(targetPath);
+          });
+        } else {
+          reject(`Error downloading file at ${url}! ${res.statusCode} - ${res.statusMessage}`);
+        }
+      });
+  });
 }
 
-export function build(options: BuildCmd): Promise<string> {
+export function compile(downloaded_path: string, module_name: string) {
+  return async () => {
+    console.log('Compiling bundle....');
+    const file = readFileSync(`${downloaded_path}/package.json`);
+    const pack = JSON.parse(file.toString());
+    const entry = `${downloaded_path}/${pack.main || 'index.js'}`;
+
+    const compiler = webpack({
+      entry,
+      output: {
+        path: `${downloaded_path}/biojs-build/`,
+        library: module_name,
+        libraryTarget: 'umd',
+      },
+      node: {
+        fs: 'empty',
+      },
+      target: 'web',
+      mode: 'none',
+    });
+    compiler.run((err: any, stats: any) => {
+      if (err || stats.hasErrors()) {
+        console.log(err);
+        throw new Error('Couldn\'t build component!');
+      }
+      console.log(`Done compiling ${module_name}`);
+    });
+  };
+}
+
+export async function install(downloaded_path: string) {
+  console.log(`Starting to install dependecies in ${downloaded_path}...`);
+  return exec('npm i', { cwd: downloaded_path })
+    .then((res) => {
+      console.log(`Done installing!`);
+      return res;
+    });
+}
+
+export async function build(options: ComponentInfo, h: ResponseToolkit): Promise<{ built: boolean, path?: string }> {
   const { module_name, module_version } = options;
   const local_path = `${process.cwd()}/${components_prefix}${module_name}/${module_version}`;
-  // check if build folder exists
-  if (folderExists(local_path)) {
-    return Promise.resolve(`${local_path}/biojs-build/build.js`);
+  const bundle_path = `${local_path}/biojs-build/${bundle_filename}`;
+
+  // TODO: Maybe store broken builts in folder names or sth and return an error on retry.
+  if (existsSync(bundle_path)) {
+    return { built: true, path: bundle_path };
+  } else if (existsSync(local_path)) {
+    // This means it's been downloaded but not yet built
+    return { built: false };
   }
+
   console.log(`No build available for ${module_name}@${module_version}`);
   console.log('Creating directory...');
   mkdirSync(local_path, { recursive: true });
-  // download if not
-  const module_path = module_name.indexOf('@') > -1 ? `${module_name}/` : '';
-  const url = `${REGISTRY_URL}/${module_path}-/${module_name}-${module_version}.tgz`;
-  return download(url, local_path)
-    .then((downloaded_path: string) => {
-      // run npm i
-      execSync('npm i', { cwd: downloaded_path });
-      // build
-      const file = readFileSync(`${downloaded_path}/package.json`);
-      const pack = JSON.parse(file.toString());
-      const entry = `${downloaded_path}/${pack.main || 'index.js'}`;
 
-      const compiler = webpack({
-        entry,
-        output: {
-          path: `${downloaded_path}/biojs-build/`,
-          library: module_name,
-          libraryTarget: 'umd',
-        },
-        node: {
-          fs: 'empty',
-        },
-        target: 'web',
-        mode: 'none',
-      });
-      compiler.run((err: any, stats: any) => {
-        if (err || stats.hasErrors()) {
-          console.log(err);
-          throw new Error('Couldn\'t build component!');
-        }
-      });
-      // return link to build.js
-      return `${downloaded_path}/biojs-build/build.js`;
+  download(module_name, module_version, local_path)
+    .then(install)
+    .then(compile(local_path, module_name))
+    .catch((err) => {
+      console.log('There was an error building the component!', err);
+      rmdirSync(local_path);
+    });
+
+  return { built: false };
+}
+
+export async function handleRequest(req: Request, h: ResponseToolkit): Promise<any> {
+  const query = req.query as any; // Compiler doesn't get this.
+  return build(query, h)
+    .then((res) => {
+      const { built, path } = res;
+      if (built && path) {
+        const stream = createReadStream(path);
+        return h.response(stream)
+          .type('application/javascript')
+          .header('Content-type', 'application/javascript');
+      } else {
+        const response = h.response('Component bundle not available. Building now...')
+          .header('Retry-After', '20')
+          .code(302);
+        return response;
+      }
     });
 }
--- src/service.ts
+++ src/service.ts
@@ -1,21 +1,42 @@
 import * as config from 'config';
-import { build, BuildCmd } from './builder';
+import { string } from 'joi';
+import { handleRequest } from './builder';
+import { Server } from 'hapi';
 
 export default async function init({
   _config = config,
 }: {
   _config?: typeof config,
-}): Promise<string> {
+}): Promise<Server> {
+
   // const module_name = 'mplexviz-ngraph';
-  const module_name = '@chgibb/angularplasmid';
+  // const module_name = '@chgibb/angularplasmid';
   // const module_name = 'cytoscape';
   // const module_version = '1.1.4';
-  const module_version = '1.0.5';
+  // const module_version = '1.0.5';
   // const module_version = '3.2.20';
-  const payload: BuildCmd = { module_name, module_version };
-  return build(payload)
-    .then((buildFile) => {
-      console.log(buildFile);
-      return buildFile;
-    });
+  // const payload: BuildCmd = { module_name, module_version };
+
+  // Hapi server
+  const server = new Server({
+    port: _config.get('server.port'),
+    routes: {
+      cors: true,
+    },
+  });
+
+  const options = {
+    validate: {
+      query: {
+        module_name: string().min(1),
+        module_version: string().min(1).default('latest'),
+      },
+    },
+  };
+
+  server.route([
+    { method: 'GET', path: '/status', handler: () => 'ok' },
+    { method: 'GET', path: '/component', options, handler: handleRequest },
+  ]);
+  return server;
 }
